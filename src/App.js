b380a32fdf1ffb8ca5062777df5e7516974aa194
Attilio Furci <lxmediacom@gmail.com>
2022-10-13 22:11:47 +0200

    dark mode

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ import { PostList } from "./components/posts";
 import jsonServerProvider from "ra-data-json-server";
 import { PostEdit } from "./components/posts/edit";
 import { PostCreate } from "./components/posts/create";
+import { defaultTheme } from "react-admin";
+
+const theme = {
+  ...defaultTheme,
+  palette: {
+    mode: "dark", // Switching the dark mode on is a single property value change.
+  },
+};
 
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 
 const App = () => (
-  <Admin dataProvider={dataProvider}>
+  <Admin dataProvider={dataProvider} theme={theme}>
     <Resource name="Users" recordRepresentation="name" list={UserList} />
     <Resource
       name="posts"
